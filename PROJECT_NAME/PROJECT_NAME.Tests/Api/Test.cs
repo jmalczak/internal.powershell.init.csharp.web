2576e0d146b34dd36a00c4dfa2e09bb2db505d2f
Jakub Malczak <jmalczak@gmail.com>
2015-10-19 19:17:28 +0900

    Added example hello world controller

--- PROJECT_NAME/PROJECT_NAME.Tests/Api/Test.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PROJECT_NAME.Tests.Api
-{
-    public class Test
-    {
-         
-    }
-}
---
+++ PROJECT_NAME/PROJECT_NAME/Api/HelloWorldController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace PROJECT_NAME.Api
+{
+    public class HelloWorldController : ApiController
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello world";
+        }
+    }
+}
